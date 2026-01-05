0c3b5f602d54dd83353ef21703ae3ecc55e88099
Mila Frans <mila.frans@student.ap.be>
2026-01-05 02:28:51 +0100

    cors fixing

--- app.js
+++ app.js
@@ -5,34 +5,46 @@ import testRoute from './routes/test.js';
 import indexRoute from './routes/index.js';
 import insectsRoute from './routes/insects.js';
 import usersRoute from './routes/users.js';
-import mongoStore from 'connect-mongo';
 import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 
-// JSON parsing
-app.use(express.json());
 
-// CORS 
 app.use(cors({
-  origin: "*",
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-// Database connect
+app.options('*', cors());
+
+
+   // JSON parsing
+
+app.use(express.json());
+
+
+// Database
+
 mongoose.connect(process.env.MONGO_URI);
 const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
+db.on('error', console.error);
 db.once('open', () => console.log('Connected to database'));
 
+
 // Routes
+
 app.use('/', indexRoute);
 app.use('/test', testRoute);
 app.use('/insects', insectsRoute);
 app.use('/users', usersRoute);
 
+
+   //Server
+ 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
